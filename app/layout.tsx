4b4106d874993e71da74b49980b785757304fffb
Timothy Finnegan <timothyfnngn@gmail.com>
2025-11-08 08:59:28 -0600

    Adding vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Space_Grotesk, JetBrains_Mono } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { Analytics } from "@vercel/analytics/next"
 
 const spaceGrotesk = Space_Grotesk({ 
   subsets: ['latin'],
